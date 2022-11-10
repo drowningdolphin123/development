edf280d87acc74d1b78a3200bab53524800d600a
Becca <rebecca_waterson@brown.edu>
2022-11-10 14:46:25 -0500

    starting on project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import shopData from "./assets/shop-data.json";
+import shopItem from "./ShopItem";
+
+/* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
+//shopData.forEach((item) => {
+// item.image = process.env.PUBLIC_URL + "/" + item.image;
+//});
+/* ############################################################## */
 
 function App() {
+  // TODO: use useState to create a state variable to hold the state of the cart
+  /* add your cart state code here */
+  const [cart, setCart] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const addToCart = (name, price) => {
+    if (cart.length == 0) {
+      setTotal(price)
+    } else {
+      const newTotal = total + price
+      setTotal(newTotal)
+    }
+    cart.push(name)
+    console.log(cart)
+  }
+
+  const removeFromCart = (name, price) => {
+    const index = cart.indexOf(name)
+    if (index > -1) {
+      cart.splice(index, 1)
+    }
+    if (cart.length != 0) {
+      setTotal(total - price)
+    }
+    if (cart.length == 0) {
+      setTotal(0)
+    }
+    console.log(cart)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="left-side">
+      <h1>Penguin Party Palace</h1> {/* TODO: personalize your bakery (if you want) */}
+
+<div className="items">
+{shopData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components 
+<div className="card" key={item.name}>
+  <shopItem {...item} />
+  <button className="button" onClick={() => addToCart(item.image, item.price)}>Add</button>
+  <button className="button" onClick={() => removeFromCart(item.image, item.price)}>Remove</button>
+</div>
+))}
+</div>
+
+      </div>
+
+      <div className="cart">
+        <h2>Cart</h2>
+        ${total}
+        {cart.map((item, index) => (
+          <img className="mini-image" src={item} />
+        ))}
+      </div>
     </div>
   );
 }
