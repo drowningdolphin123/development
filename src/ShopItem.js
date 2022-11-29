0eb5569a64a317bdb45a17446c186d8ebb56c7db
Becca <rebecca_waterson@brown.edu>
2022-11-29 12:24:11 -0500

    mostly working

--- src/ShopItem.js
+++ src/ShopItem.js
@@ -4,10 +4,10 @@ import "./BakeryItemCss.css"
 export default function BakeryItem(props) {
   return (
     <div className="BakeryItem">
-        <img src={props.image} width="300px;"></img>
+        <img src={props.image} width="60px"></img>
         <div className="container">
         <b>{props.name}</b> <br></br>
-        <u>{props.price}</u>
+        <u>${props.price}</u>
       </div>
     </div>
   );
